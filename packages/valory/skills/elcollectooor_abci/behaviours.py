36ffe83963edd0f530ef9264f258b0130ee28e2d
Ardian <ardianabazi09@gmail.com>
2022-01-24 23:28:42 +0100

    fix: removed unused vars, removed default callback

--- packages/valory/skills/elcollectooor_abci/behaviours.py
+++ packages/valory/skills/elcollectooor_abci/behaviours.py
@@ -150,24 +150,13 @@ class ObservationRoundBehaviour(ElCollectooorABCIBaseState):
     state_id = "observation"
     matching_round = ObservationRound
 
-    def __init__(self, *args: Any, **kwargs: Any):
+    def __init__(self, **kwargs: Any):
         """Init the observing behaviour."""
         super().__init__(**kwargs)
-        # TODO: not all vars are necessary
-        self.max_eth_in_wei = kwargs.pop("max_eth_in_wei", 1000000000000000000)
-        self.safe_tx_gas = kwargs.pop("safe_tx_gas", 4000000)
         self.artblocks_contract = kwargs.pop(
             "artblocks_contract", "0x1CD623a86751d4C4f20c96000FEC763941f098A2"
         )
-        self.artblocks_periphery_contract = kwargs.pop(
-            "artblocks_periphery_contract", "0x58727f5Fc3705C30C9aDC2bcCC787AB2BA24c441"
-        )
-        self.safe_contract = kwargs.pop(
-            "safe_contract", "0x2caB92c1E9D2a701Ca0411b0ff35A0907Ca31F7f"
-        )
-        self.seconds_between_periods = kwargs.pop("seconds_between_periods", 30)
-        self.starting_id = kwargs.pop("starting_id", 0)
-
+        self.starting_id = kwargs.pop("starting_id", None)
         self.max_retries = kwargs.pop("max_retries", 5)
         self._retries_made = 0
 
@@ -176,7 +165,7 @@ class ObservationRoundBehaviour(ElCollectooorABCIBaseState):
 
         if self.is_retries_exceeded():
             with benchmark_tool.measure(
-                self,
+                    self,
             ).consensus():
                 yield from self.wait_until_round_end()
 
@@ -185,44 +174,51 @@ class ObservationRoundBehaviour(ElCollectooorABCIBaseState):
             return
 
         with benchmark_tool.measure(
-            self,
-        ).local():
-            # fetch an active project
-            response = yield from self.get_contract_api_response(
-                request_callback=self.default_callback_request,
-                performative=ContractApiMessage.Performative.GET_STATE,
-                contract_address=self.artblocks_contract,
-                contract_id=str(ArtBlocksContract.contract_id),
-                contract_callable="get_active_project",
-                starting_id=self.starting_id,
-            )
-
-            # response body also has project details
-            project_details = response.state.body
-            project_id = (
-                project_details["project_id"]
-                if "project_id" in project_details.keys()
-                else None
-            )
-
-        if project_id:
-            self.context.logger.info(f"Retrieved project id: {project_id}.")
-            payload = ObservationPayload(
-                self.context.agent_address,
-                project_details,
-            )
-
-            with benchmark_tool.measure(
                 self,
-            ).consensus():
-                yield from self.send_a2a_transaction(payload)
-                yield from self.wait_until_round_end()
-        else:
-            self.context.logger.error(
-                "project_id couldn't be extracted from contract response"
-            )
-            yield from self.sleep(self.params.sleep_time)
-            self._increment_retries()
+        ).local():
+            try:
+                # fetch an active project
+                response = yield from self.get_contract_api_response(
+                    performative=ContractApiMessage.Performative.GET_STATE,
+                    contract_address=self.artblocks_contract,
+                    contract_id=str(ArtBlocksContract.contract_id),
+                    contract_callable="get_active_project",
+                    starting_id=self.starting_id,
+                )
+
+                # response body also has project details
+                enforce(
+                    response is not None and response.state is not None and response.state.body is not None,
+                    "response, response.state, response.state.body must exist"
+                )
+
+                project_details = response.state.body
+
+                enforce(
+                    "project_id" in project_details.keys(),
+                    "project_details was none, or project_id was not found in project_details"
+                )
+
+                project_id = project_details["project_id"]
+
+                self.context.logger.info(f"Retrieved project id: {project_id}.")
+                payload = ObservationPayload(
+                    self.context.agent_address,
+                    project_details,
+                )
+
+                with benchmark_tool.measure(
+                        self,
+                ).consensus():
+                    yield from self.send_a2a_transaction(payload)
+                    yield from self.wait_until_round_end()
+
+            except AEAEnforceError:
+                self.context.logger.error(
+                    "project_id couldn't be extracted from contract response"
+                )
+                yield from self.sleep(self.params.sleep_time)
+                self._increment_retries()
 
         self.set_done()
 
@@ -256,7 +252,7 @@ class DetailsRoundBehaviour(ElCollectooorABCIBaseState):
         """The details act"""
 
         with benchmark_tool.measure(
-            self,
+                self,
         ).local():
             # fetch an active project
             most_voted_project = json.loads(self.period_state.most_voted_project)
@@ -280,7 +276,7 @@ class DetailsRoundBehaviour(ElCollectooorABCIBaseState):
             )
 
         with benchmark_tool.measure(
-            self,
+                self,
         ).consensus():
             yield from self.send_a2a_transaction(payload)
             yield from self.wait_until_round_end()
@@ -293,7 +289,6 @@ class DetailsRoundBehaviour(ElCollectooorABCIBaseState):
         )
 
         response = yield from self.get_contract_api_response(
-            request_callback=self.default_callback_request,
             performative=ContractApiMessage.Performative.GET_STATE,
             contract_address=self.artblocks_contract,
             contract_id=str(ArtBlocksContract.contract_id),
@@ -319,7 +314,7 @@ class DecisionRoundBehaviour(ElCollectooorABCIBaseState):
     def async_act(self) -> Generator:
         """The Decision act"""
         with benchmark_tool.measure(
-            self,
+                self,
         ).local():
             # fetch an active project
             most_voted_project = json.loads(self.period_state.most_voted_project)
@@ -337,7 +332,7 @@ class DecisionRoundBehaviour(ElCollectooorABCIBaseState):
             )
 
         with benchmark_tool.measure(
-            self,
+                self,
         ).consensus():
             yield from self.send_a2a_transaction(payload)
             yield from self.wait_until_round_end()
@@ -345,7 +340,7 @@ class DecisionRoundBehaviour(ElCollectooorABCIBaseState):
         self.set_done()
 
     def _make_decision(
-        self, project_details: dict, most_voted_details: List[dict]
+            self, project_details: dict, most_voted_details: List[dict]
     ) -> int:
         """Method that decides on an outcome"""
         decision_model = DecisionModel()
@@ -370,32 +365,20 @@ class TransactionRoundBehaviour(ElCollectooorABCIBaseState):
     state_id = "transaction_collection"
     matching_round = TransactionRound
 
-    def __init__(self, *args: Any, **kwargs: Any):
+    def __init__(self, **kwargs: Any):
         """Init the observing behaviour."""
         super().__init__(**kwargs)
-        # TODO: not all vars are necessary
-        self.max_eth_in_wei = kwargs.pop("max_eth_in_wei", 1000000000000000000)
-        self.safe_tx_gas = kwargs.pop("safe_tx_gas", 4000000)
-        self.artblocks_contract = kwargs.pop(
-            "artblocks_contract", "0x1CD623a86751d4C4f20c96000FEC763941f098A2"
-        )
         self.artblocks_periphery_contract = kwargs.pop(
             "artblocks_periphery_contract", "0x58727f5Fc3705C30C9aDC2bcCC787AB2BA24c441"
         )
-        self.safe_contract = kwargs.pop(
-            "safe_contract", "0x2caB92c1E9D2a701Ca0411b0ff35A0907Ca31F7f"
-        )
-        self.seconds_between_periods = kwargs.pop("seconds_between_periods", 30)
-        self.starting_id = kwargs.pop("starting_id", 0)
         self.max_retries = kwargs.pop("max_retries", 5)
-        self._retries_made = 0
 
     def async_act(self) -> Generator:
         """Implement the act."""
 
         if self.is_retries_exceeded():
             with benchmark_tool.measure(
-                self,
+                    self,
             ).consensus():
                 yield from self.wait_until_round_end()
 
@@ -404,17 +387,17 @@ class TransactionRoundBehaviour(ElCollectooorABCIBaseState):
             return
 
         with benchmark_tool.measure(
-            self,
+                self,
         ).local():
             # we extract the project_id from the frozen set, and throw an error if it doest exist
             project_id = json.loads(self.period_state.most_voted_project)["project_id"]
+
             enforce(
                 project_id is not None,
                 "couldn't find project_id, or project_id is None",
             )
 
             response = yield from self.get_contract_api_response(
-                request_callback=self.default_callback_request,
                 performative=ContractApiMessage.Performative.GET_STATE,
                 contract_address=self.artblocks_periphery_contract,
                 contract_id=str(ArtBlocksPeripheryContract.contract_id),
@@ -437,7 +420,7 @@ class TransactionRoundBehaviour(ElCollectooorABCIBaseState):
                 data,
             )
             with benchmark_tool.measure(
-                self,
+                    self,
             ).consensus():
                 yield from self.send_a2a_transaction(payload)
                 yield from self.wait_until_round_end()
