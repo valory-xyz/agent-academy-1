0b382f6380cd77b395a92fb38c572232bcfaa146
Ardian <ardianabazi09@gmail.com>
2022-02-15 09:46:25 +0100

    feat: compiled & scaffolded Token Vault & Factory, Basket & Factory,

---
+++ packages/valory/contracts/basket_factory/contract.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+# ------------------------------------------------------------------------------
+#
+#   Copyright 2021-2022 Valory AG
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
+# ------------------------------------------------------------------------------
+
+"""This module contains the class to connect to a Fractional Basket Factory contract."""
+import binascii
+import logging
+import secrets
+from enum import Enum
+from typing import Any, Dict, List, Optional, Tuple, Union, cast
+
+from aea.common import JSONLike
+from aea.configurations.base import PublicId
+from aea.contracts.base import Contract
+from aea.crypto.base import LedgerApi
+from aea_ledger_ethereum import EthereumApi
+from eth_typing import ChecksumAddress, HexAddress, HexStr
+from hexbytes import HexBytes
+from packaging.version import Version
+from py_eth_sig_utils.eip712 import encode_typed_data
+from requests import HTTPError
+from web3.exceptions import SolidityError, TransactionNotFound
+from web3.types import Nonce, TxData, TxParams, Wei
+
+from packages.valory.contracts.gnosis_safe_proxy_factory.contract import (
+    GnosisSafeProxyFactoryContract,
+)
+
+PUBLIC_ID = PublicId.from_str("valory/basket_factory:0.1.0")
+
+_logger = logging.getLogger(
+    f"aea.packages.{PUBLIC_ID.author}.contracts.{PUBLIC_ID.name}.contract"
+)
+
+
+class BasketFactoryContract(Contract):
+    """The Basket Factory contract."""
+
+    contract_id = PUBLIC_ID
+
+    @classmethod
+    def get_raw_transaction(cls, ledger_api: LedgerApi, contract_address: str, **kwargs: Any) -> Optional[JSONLike]:
+        raise NotImplementedError
+
+    @classmethod
+    def get_raw_message(cls, ledger_api: LedgerApi, contract_address: str, **kwargs: Any) -> Optional[bytes]:
+        raise NotImplementedError
+
+    @classmethod
+    def get_state(cls, ledger_api: LedgerApi, contract_address: str, **kwargs: Any) -> Optional[JSONLike]:
+        raise NotImplementedError
