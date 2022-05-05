dd2a2079fcfb931c437bee8e197e07d8c8437244
David Minarsch <david.minarsch@googlemail.com>
2022-05-04 19:53:36 -0700

    chore: update test case

--- break_abci.py
+++ break_abci.py
@@ -1,11 +1,14 @@
-from tests.test_packages.test_connections.fuzzy_tests.mock_node.node import (
+#!/usr/bin/env python3
+from tests.test_connections.fuzzy_tests.mock_node.node import (
     MockNode
 )
-from tests.test_packages.test_connections.fuzzy_tests.mock_node.channels.tcp_channel import (
+from tests.test_connections.fuzzy_tests.mock_node.channels.tcp_channel import (
     TcpChannel,
 )
 
-mock_tendermint = MockNode(TcpChannel())
+channel = TcpChannel()
+channel.connect()
+mock_tendermint = MockNode(channel)
 
 data = 'a' * 100000
 mock_tendermint.check_tx(data.encode(), False)
