7b5d200e4ed06a2f2060230712dbd33fc2425c00
MarcoFavorito <marco.favorito@gmail.com>
2022-05-06 10:44:22 +0200

    minor change to break_abci.py
    Add the usual guard 'if __name__ == "__main__"'

--- break_abci.py
+++ break_abci.py
@@ -6,9 +6,12 @@ from tests.test_connections.fuzzy_tests.mock_node.channels.tcp_channel import (
     TcpChannel,
 )
 
-channel = TcpChannel()
-channel.connect()
-mock_tendermint = MockNode(channel)
 
-data = 'a' * 100000
-mock_tendermint.check_tx(data.encode(), False)
+if __name__ == '__main__':
+    channel = TcpChannel()
+    mock_tendermint = MockNode(channel)
+
+    data = 'a' * 100000
+    mock_tendermint.connect()
+    mock_tendermint.check_tx(data.encode(), False)
+    mock_tendermint.disconnect()
