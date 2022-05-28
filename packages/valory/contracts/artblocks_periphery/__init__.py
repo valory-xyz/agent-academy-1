66380ef0f3f46ad42aa7f139780d1c8b72366399
Ardian <ardianabazi09@gmail.com>
2022-05-28 18:19:30 +0200

    feat: updated periphery contract to the newest interface

--- packages/valory/contracts/artblocks_periphery/__init__.py
+++ packages/valory/contracts/artblocks_periphery/__init__.py
@@ -17,4 +17,4 @@
 #
 # ------------------------------------------------------------------------------
 
-"""This module contains the support resources for the artblocks periphery contract."""
+"""This module contains the support resources for the artblocks periphery contracts."""
