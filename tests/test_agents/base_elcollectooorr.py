daed2a529483db68bec8c41ea77993cd8d4af52a
Ardian <ardianabazi09@gmail.com>
2022-07-06 16:21:34 +0200

    feat: added elcol configs and helper methods

--- tests/test_agents/base_elcollectooorr.py
+++ tests/test_agents/base_elcollectooorr.py
@@ -18,51 +18,218 @@
 # ------------------------------------------------------------------------------
 
 """End2end tests base classes for this repo."""
+import logging
+import subprocess
+import threading
+import time
 
+import web3
 from aea.configurations.base import PublicId
+from click.testing import Result
+from web3.types import Wei
 
 from tests.helpers.constants import ARTBLOCKS_ADDRESS as _DEFAULT_ARTBLOCKS_ADDRESS
 from tests.helpers.constants import (
-    ARTBLOCKS_PERIPHERY_ADDRESS as _DEFAULT_ARTBLOCKS_PERIPHERY_ADDRESS,
+    ARTBLOCKS_FILTER_ADDRESS as _DEFAULT_ARTBLOCKS_FILTER_ADDRESS,
 )
-from tests.helpers.constants import DECISION_MODEL_TYPE as _DEFAULT_DECISION_MODEL_TYPE
-from tests.helpers.constants import TARGET_PROJECT_ID as _DEFAULT_TARGET_PROJECT_ID
+from tests.helpers.constants import (
+    BASKET_FACTORY_ADDRESS as _DEFAULT_BASKET_FACTORY_ADDRESS,
+)
+from tests.helpers.constants import ELCOL_NET_CHAIN_ID as _DEFAULT_ELCOL_NET_CHAIN_ID
+from tests.helpers.constants import ELCOL_NET_HOST as _DEFAULT_ELCOL_NET_HOST
+from tests.helpers.constants import (
+    HARDHAT_ELCOL_KEY_PAIRS as _DEFAULT_HARDHAT_ELCOL_KEY_PAIRS,
+)
+from tests.helpers.constants import HTTP_LOCALHOST as _DEFAULT_HTTP_LOCALHOST
+from tests.helpers.constants import (
+    MOCK_ARTBLOCKS_API_PORT as _DEFAULT_MOCK_ARTBLOCKS_API_PORT,
+)
+from tests.helpers.constants import MULTISEND_ADDRESS as _DEFAULT_MULTISEND_ADDRESS
+from tests.helpers.constants import (
+    SAFE_CALLBACK_HANDLER as _DEFAULT_SAFE_CALLBACK_HANDLER,
+)
+from tests.helpers.constants import (
+    SAFE_CONTRACT_ADDRESS as _DEFAULT_SAFE_CONTRACT_ADDRESS,
+)
+from tests.helpers.constants import (
+    SAFE_FACTORY_ADDRESS as _DEFAULT_SAFE_FACTORY_ADDRESS,
+)
+from tests.helpers.constants import SETTINGS_ADRESS as _DEFAULT_SETTINGS_ADDRESS
+from tests.helpers.constants import (
+    TOKEN_VAULT_FACTORY_ADDRESS as _DEFAULT_TOKEN_VAULT_FACTORY_ADDRESS,
+)
 from tests.helpers.tendermint_utils import TendermintNodeInfo
-from tests.test_agents.base import BaseTestEnd2EndNormalExecution
+from tests.test_agents.base import BaseTestEnd2End
 
 
-class BaseTestElCollectooorrEnd2End(BaseTestEnd2EndNormalExecution):
+class BaseTestElCollectooorrEnd2End(BaseTestEnd2End):
     """
     Extended base class for conducting E2E tests with the El Collectooorr.
 
     Test subclasses must set NB_AGENTS, agent_package, wait_to_finish and check_strings.
     """
-
-    STARTING_PROJECT_ID = _DEFAULT_TARGET_PROJECT_ID + 1
+    cli_log_options = ["-v", "INFO"]  # no need for debug
     ARTBLOCKS_ADDRESS = _DEFAULT_ARTBLOCKS_ADDRESS
-    ARTBLOCKS_PERIPHERY_ADDRESS = _DEFAULT_ARTBLOCKS_PERIPHERY_ADDRESS
-    DECISION_MODEL_TYPE = _DEFAULT_DECISION_MODEL_TYPE
+    ARTBLOCKS_FILTER_ADDRESS = _DEFAULT_ARTBLOCKS_FILTER_ADDRESS
+    SAFE_CONTRACT_ADDRESS = _DEFAULT_SAFE_CONTRACT_ADDRESS
+    SAFE_CALLBACK_HANDLER = _DEFAULT_SAFE_CALLBACK_HANDLER
+    SAFE_FACTORY_ADDRESS = _DEFAULT_SAFE_FACTORY_ADDRESS
+    BASKET_FACTORY_ADDRESS = _DEFAULT_BASKET_FACTORY_ADDRESS
+    TOKEN_VAULT_FACTORY_ADDRESS = _DEFAULT_TOKEN_VAULT_FACTORY_ADDRESS
+    SETTINGS_ADRESS = _DEFAULT_SETTINGS_ADDRESS
+    MULTISEND_ADDRESS = _DEFAULT_MULTISEND_ADDRESS
+    MOCK_ARTBLOCKS_API_PORT = _DEFAULT_MOCK_ARTBLOCKS_API_PORT
+    HTTP_LOCALHOST = _DEFAULT_HTTP_LOCALHOST
+    HARDHAT_ELCOL_KEY_PAIRS = _DEFAULT_HARDHAT_ELCOL_KEY_PAIRS
+    ELCOL_NET_HOST = _DEFAULT_ELCOL_NET_HOST
+    ELCOL_NET_CHAIN_ID = _DEFAULT_ELCOL_NET_CHAIN_ID
 
-    def __set_configs(self, node: TendermintNodeInfo) -> None:
-        """Set the current agent's config overrides."""
-        super().__set_configs(node)
-        self.set_config(
-            f"vendor.valory.skills.{PublicId.from_str(self.skill_package).name}.models.params.args.starting_project_id",
-            self.STARTING_PROJECT_ID,
+    def test_run(self) -> None:
+        """Run the ABCI skill."""
+        for agent_id in range(self.NB_AGENTS):
+            self._launch_agent_i(agent_id)
+
+        logging.info("Waiting Tendermint nodes to be up")
+        self.health_check(
+            self.tendermint_net_builder,
+            max_retries=self.HEALTH_CHECK_MAX_RETRIES,
+            sleep_interval=self.HEALTH_CHECK_SLEEP_INTERVAL,
         )
+        thread = threading.Thread(target=self._deposit_to_safe_contract)
+        thread.start()
+        self._check_aea_messages()
+
+    def _BaseTestEnd2End__set_configs(self, node: TendermintNodeInfo) -> None:
+        """Set the current agent's config overrides."""
+        super()._BaseTestEnd2End__set_configs(node)  # type: ignore
+
         self.set_config(
             f"vendor.valory.skills.{PublicId.from_str(self.skill_package).name}.models.params.args.artblocks_contract",
             self.ARTBLOCKS_ADDRESS,
         )
         self.set_config(
-            f"vendor.valory.skills.{PublicId.from_str(self.skill_package).name}.models.params.args.artblocks_periphery_contract",
-            self.ARTBLOCKS_PERIPHERY_ADDRESS,
+            f"vendor.valory.skills.{PublicId.from_str(self.skill_package).name}.models.params.args.artblocks_minter_filter",
+            self.ARTBLOCKS_FILTER_ADDRESS,
         )
         self.set_config(
-            f"vendor.valory.skills.{PublicId.from_str(self.skill_package).name}.models.params.args.artblocks_periphery_contract",
-            self.ARTBLOCKS_PERIPHERY_ADDRESS,
+            f"vendor.valory.skills.{PublicId.from_str(self.skill_package).name}.models.params.args.basket_factory_address",
+            self.BASKET_FACTORY_ADDRESS,
         )
         self.set_config(
-            f"vendor.valory.skills.{PublicId.from_str(self.skill_package).name}.models.params.args.decision_model_type",
-            self.DECISION_MODEL_TYPE,
+            f"vendor.valory.skills.{PublicId.from_str(self.skill_package).name}.models.params.args.token_vault_factory_address",
+            self.TOKEN_VAULT_FACTORY_ADDRESS,
         )
+        self.set_config(
+            f"vendor.valory.skills.{PublicId.from_str(self.skill_package).name}.models.params.args.settings_address",
+            self.SETTINGS_ADRESS,
+        )
+        self.set_config(
+            f"vendor.valory.skills.{PublicId.from_str(self.skill_package).name}.models.params.args.multisend_address",
+            self.MULTISEND_ADDRESS,
+        )
+        self._replace_default_addresses()
+
+    def _replace_default_addresses(self) -> None:
+        """Update the gnosis safe contract default addresses."""
+        for agent_name in self.agent_names:
+            try:
+                with open(
+                    self.t.joinpath(agent_name).joinpath(
+                        "vendor/valory/contracts/gnosis_safe/contract.py"
+                    ),
+                    "r",
+                ) as f:
+                    org = f.read()
+            except Exception:
+                # happens when the agent is not yet fetched
+                continue
+
+            dst = (
+                org.replace(
+                    'SAFE_CONTRACT = "0xd9Db270c1B5E3Bd161E8c8503c55cEABeE709552"',
+                    f'SAFE_CONTRACT = "{self.SAFE_CONTRACT_ADDRESS}"',
+                )
+                .replace(
+                    'DEFAULT_CALLBACK_HANDLER = "0xf48f2B2d2a534e402487b3ee7C18c33Aec0Fe5e4"',
+                    f'DEFAULT_CALLBACK_HANDLER = "{self.SAFE_CALLBACK_HANDLER}"',
+                )
+                .replace(
+                    'PROXY_FACTORY_CONTRACT = "0xa6B71E26C5e0845f74c812102Ca7114b6a896AB2"',
+                    f'PROXY_FACTORY_CONTRACT = "{self.SAFE_FACTORY_ADDRESS}"',
+                )
+                .replace(
+                    "return dict(verified=verified)",
+                    "return dict(verified=True)",
+                )
+            )
+
+            with open(
+                self.t.joinpath(agent_name).joinpath(
+                    "vendor/valory/contracts/gnosis_safe/contract.py"
+                ),
+                "w",
+            ) as f:
+                f.write(dst)
+                f.flush()
+
+    @classmethod
+    def run_install(cls) -> Result:
+        """
+        Execute AEA CLI install command.
+
+        Run from agent's directory.
+
+        :return: Result
+        """
+        return cls.run_cli_command(*("-s", "install"), cwd=cls._get_cwd())
+
+    @classmethod
+    def run_agent(cls, *args: str) -> subprocess.Popen:
+        """
+        Run agent as subprocess.
+
+        Run from agent's directory.
+
+        :param args: CLI args
+
+        :return: subprocess object.
+        """
+        return cls._start_cli_process(*("-s", "run"), *args)
+
+    def _deposit_to_safe_contract(self, timeout: int = 200) -> None:
+        """This method simulates a user depositing funds into the safe contract."""
+        start = time.time()
+        while True:
+            for output in self.stdout.values():
+                safe_contract_address_parts = output.split("Safe contract address: ", 1)
+                if len(safe_contract_address_parts) > 1:
+                    target_address = (
+                        safe_contract_address_parts[1]
+                        .split(" ", 1)[0]
+                        .split("\n")[0]
+                        .strip()
+                    )
+                    instance = web3.Web3(web3.HTTPProvider(self.ELCOL_NET_HOST))
+                    sender_address, private_key = self.HARDHAT_ELCOL_KEY_PAIRS[0]
+                    raw_tx = instance.eth.send_transaction(
+                        {
+                            "to": target_address,
+                            "from": sender_address,
+                            "value": Wei(10 ** 18),
+                            "chainId": self.ELCOL_NET_CHAIN_ID,
+                            "gasPrice": instance.eth.gas_price,
+                            "nonce": instance.eth.getTransactionCount(
+                                instance.toChecksumAddress(sender_address)
+                            ),
+                        }
+                    )
+                    signed_tx = instance.eth.account.signTransaction(
+                        raw_tx, private_key=private_key
+                    )
+                    instance.eth.sendRawTransaction(signed_tx.rawTransaction)
+                    return
+
+            if time.time() - start > timeout:
+                return
+
+            time.sleep(5)  # wait 5 sec before checking again
